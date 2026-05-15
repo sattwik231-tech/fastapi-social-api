d9b3b17add19c8f191ceb1011c6c4d9e44b26a0e
Sattwik Das <sattwik231@gmail.com>
2026-05-15 14:13:45 +0530

    Initial backend setup with FastAPI and PostgreSQL

---
+++ app/schemas.py
@@ -0,0 +1,55 @@
+from pydantic import BaseModel, EmailStr, conint, constr
+from datetime import datetime
+
+
+class PostBase(BaseModel):
+    title: str
+    content: str 
+    published: bool = True
+
+class PostCreate(PostBase):
+    pass
+
+
+class UserOut(BaseModel):
+    id: int
+    email: EmailStr
+    created_at: datetime
+
+
+class Post(PostBase):
+    id: int
+    created_at: datetime
+    owner_id : int
+    owner: UserOut
+
+    class Config:
+        orm_mode = True
+
+class PostOut(BaseModel):
+    Post : Post
+    votes : int
+
+    class Config:
+        orm_mode = True
+
+class UserCreate(BaseModel):
+    email: EmailStr
+    password: constr(min_length=6, max_length=72) # type: ignore
+
+
+class UserLogin(BaseModel):
+    email: EmailStr
+    password : str
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+class TokenData(BaseModel):
+    id: int
+
+class Vote(BaseModel):
+    post_id: int
+    dir: conint(ge=0, le=1) # type: ignore
+
